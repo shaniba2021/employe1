75dbf810a2b251ae8d9ba7de13f3e6c072780c35
shaniba2021 <saniba1234@gmail.com>
2021-04-27 09:47:55 +0530

    third commit

--- __manifest__.py
+++ __manifest__.py
@@ -26,7 +26,6 @@
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/deptviews.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
